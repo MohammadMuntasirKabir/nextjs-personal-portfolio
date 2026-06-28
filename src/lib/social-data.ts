c3012cc9d912f09a88e004e94d6bc95e22952646
MuntaKabir <mohammad.muntasir.kabir@gmail.com>
2026-06-28 14:12:44 +0600

    Remove X (Twitter) social link

--- src/lib/social-data.ts
+++ src/lib/social-data.ts
@@ -1,11 +1,9 @@
 import { GithubIcon, LinkedinIcon, FacebookIcon } from "./social-links";
-import { X } from "lucide-react";
 
 export const socialLinks = [
   { icon: GithubIcon, href: "https://github.com/MohammadMuntasirKabir", label: "GitHub" },
   { icon: LinkedinIcon, href: "https://www.linkedin.com/in/mohammad-muntasir-kabir-642020381/", label: "LinkedIn" },
   { icon: FacebookIcon, href: "https://www.facebook.com/muntasir.kabir.rownak/", label: "Facebook" },
-  { icon: X, href: "https://twitter.com/rownak", label: "X (Twitter)" },
 ];
 
 export const footerLinks = [
--- src/lib/social-links.tsx
+++ src/lib/social-links.tsx
@@ -1,5 +1,3 @@
-import { X } from "lucide-react";
-
 export function GithubIcon({ className }: { className?: string }) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
